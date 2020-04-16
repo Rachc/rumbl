632cf4a24f6862f0d8c245fb32026ce294d58e4e
Rachel Curioso <rachel.curioso@gmail.com>
2020-04-16 09:33:56 -0300

    chapters 1 to 5

---
+++ lib/rumbl/accounts/user.ex
@@ -0,0 +1,29 @@
+defmodule Rumbl.Accounts.User do
+  # defstruct [:id, :name, :username]
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Rumbl.Accounts.Credential
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    has_one :credential, Credential
+
+    timestamps()
+  end
+
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :username])
+    |> validate_required([:name, :username])
+    |> validate_length(:username, min: 1, max: 20)
+  end
+
+  # Changeset novo para lidar com dados sensíveis
+  def registration_changeset(user, params) do
+    user
+    |> changeset(params)
+    |> cast_assoc(:credential, with: &Credential.changeset/2, required: true)
+  end
+end
