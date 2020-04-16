99fb410c4c9b29516003a8b48c6e46690610a29e
Rachel Curioso <rachel.curioso@gmail.com>
2020-04-16 09:44:46 -0300

    Generate Video module with mix phx.gen.html

---
+++ lib/rumbl/multimedia/video.ex
@@ -0,0 +1,19 @@
+defmodule Rumbl.Multimedia.Video do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "videos" do
+    field :description, :string
+    field :url, :string
+    field :user_id, :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(video, attrs) do
+    video
+    |> cast(attrs, [:url, :description])
+    |> validate_required([:url, :description])
+  end
+end
