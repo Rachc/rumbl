e6b42939ca034fb43733f8801eed24f0c33f7aab
Rachel Curioso <rachel.curioso@gmail.com>
2020-04-17 09:12:33 -0300

    adds title field to video

--- lib/rumbl/multimedia/video.ex
+++ lib/rumbl/multimedia/video.ex
@@ -4,12 +4,17 @@ defmodule Rumbl.Multimedia.Video do
 
   schema "videos" do
     field :description, :string
+    field :title, :string
     field :url, :string
     field :user_id, :id
 
     timestamps()
   end
 
+  @spec changeset(
+          {map, map} | %{:__struct__ => atom | %{__changeset__: map}, optional(atom) => any},
+          :invalid | %{optional(:__struct__) => none, optional(atom | binary) => any}
+        ) :: Ecto.Changeset.t()
   @doc false
   def changeset(video, attrs) do
     video
